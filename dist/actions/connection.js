af3278acf1471eb705786b125a3b50385f17ad23
mmoutenot <mmoutenot@gmail.com>
2017-02-08 14:31:21 -0500

    add connection actions

--- dist/actions/connection.js
+++ dist/actions/connection.js
@@ -1,1 +1,36 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.watchConnection = exports.unWatchConnection = undefined;
+
+var _constants = require('../constants');
+
+/**
+ * @description Remove listener from user profile
+ * @param {Object} firebase - Internal firebase object
+ */
+var unWatchConnection = exports.unWatchConnection = function unWatchConnection(firebase) {
+  if (firebase._.connectionWatch) {
+    firebase.database().ref(".info/connected").off('value', firebase._.connectionWatch);
+    firebase._.connectionWatch = null;
+  }
+};
+
+/**
+ * @description Watch user profile
+ * @param {Function} dispatch - Action dispatch function
+ * @param {Object} firebase - Internal firebase object
+ */
+var watchConnection = exports.watchConnection = function watchConnection(dispatch, firebase) {
+  unWatchConnection(firebase);
+  firebase._.connectionWatch = firebase.database().ref(".info/connected").on('value', function (snap) {
+    dispatch({
+      type: _constants.SET_CONNECTED,
+      isConnected: snap.val()
+    });
+  });
+};
+
+exports.default = { watchConnection: watchConnection, unWatchConnection: unWatchConnection };
---
+++ src/actions/connection.js
@@ -0,0 +1,32 @@
+import {SET_CONNECTED} from '../constants';
+
+/**
+ * @description Remove listener from user profile
+ * @param {Object} firebase - Internal firebase object
+ */
+export const unWatchConnection = (firebase) => {
+  if (firebase._.connectionWatch) {
+    firebase.database().ref(".info/connected").off('value', firebase._.connectionWatch)
+    firebase._.connectionWatch = null
+  }
+}
+
+/**
+ * @description Watch user profile
+ * @param {Function} dispatch - Action dispatch function
+ * @param {Object} firebase - Internal firebase object
+ */
+export const watchConnection = (dispatch, firebase) => {
+  unWatchConnection(firebase)
+  firebase._.connectionWatch = firebase.database()
+    .ref(".info/connected")
+    .on('value', snap => {
+      dispatch({
+        type: SET_CONNECTED,
+        isConnected: snap.val()
+      })
+    })
+}
+
+export default { watchConnection, unWatchConnection }
+
